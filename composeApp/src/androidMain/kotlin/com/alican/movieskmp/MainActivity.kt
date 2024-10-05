303ed0eb615ee7434473464e1051d1db2856a1d8
alicansekban <alicansekban@hotmail.com>
2024-10-05 22:55:36 +0300

    padding added

--- composeApp/src/androidMain/kotlin/com/alican/movieskmp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/alican/movieskmp/MainActivity.kt
@@ -3,13 +3,24 @@ package com.alican.movieskmp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.ui.Modifier
 import ui.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App()
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .windowInsetsPadding(WindowInsets.safeDrawing)) {
+                App()
+            }
         }
     }
 }
