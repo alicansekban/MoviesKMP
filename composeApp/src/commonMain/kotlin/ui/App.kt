64dee4a4f7a783e91995e92a183ad32c3831865e
alicansekban <alicansekban@hotmail.com>
2024-10-12 17:43:19 +0300

    bottom bar visibility logic added

--- composeApp/src/commonMain/kotlin/ui/App.kt
+++ composeApp/src/commonMain/kotlin/ui/App.kt
@@ -5,24 +5,41 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import components.bottom_bar.BottomBar
 import components.navigation.MainNavigation
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinContext
+import utils.FavoritesRoute
+import utils.HomeRoute
+import utils.SearchRoute
 
 @Composable
 @Preview
 fun App() {
 
     val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val showBottomBar by remember {
+        derivedStateOf {
+            navBackStackEntry?.destination?.hasRoute<SearchRoute>() == true ||
+                    navBackStackEntry?.destination?.hasRoute<FavoritesRoute>() == true ||
+                    navBackStackEntry?.destination?.hasRoute<HomeRoute>() == true
+        }
+    }
+
     MaterialTheme {
         KoinContext {
             Scaffold(
                 modifier = Modifier.fillMaxSize(),
                 bottomBar = {
-                    BottomBar(navController, isBottomBarVisible = true)
+                    BottomBar(navController, isBottomBarVisible = showBottomBar)
                 }
             ) { paddingValues ->
                 MainNavigation(navController, modifier = Modifier.padding(paddingValues))
