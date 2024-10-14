053bddd85f3861db7481ec8d0eadce6a0835a830
alicansekban <alicansekban@hotmail.com>
2024-10-14 18:39:03 +0300

    added room db with koin

--- composeApp/src/commonMain/kotlin/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/di/AppModule.kt
@@ -1,14 +1,17 @@
 package di
 
 import org.koin.core.context.startKoin
+import org.koin.core.module.Module
 import org.koin.dsl.KoinAppDeclaration
+
+expect fun platformModule(): Module
 
 fun initKoin(config: KoinAppDeclaration? = null) {
     startKoin {
         config?.invoke(this)
         modules(
             provideHttpClientModule, provideRepositoryModule, provideViewModelModule,
-            interactorModule, serviceModule
+            interactorModule, serviceModule, platformModule()
         )
     }
 }
