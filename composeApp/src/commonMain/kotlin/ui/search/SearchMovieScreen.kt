f3a132e368378a7547009c891401155143d4d02c
alicansekban <alicansekban@hotmail.com>
2024-10-17 23:23:31 +0300

    added top bar for search screen

--- composeApp/src/commonMain/kotlin/ui/search/SearchMovieScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/search/SearchMovieScreen.kt
@@ -1,5 +1,6 @@
 package ui.search
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,26 +14,27 @@ import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Text
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import components.button.ListResetButton
 import components.dialog.LoadingDialog
 import components.movie.MovieListItem
+import components.top_bar.CustomTopBar
+import kotlinx.coroutines.launch
 import movieskmp.composeapp.generated.resources.Res
 import movieskmp.composeapp.generated.resources.search_placeholder
+import movieskmp.composeapp.generated.resources.search_title
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.annotation.KoinExperimentalAPI
@@ -45,6 +47,7 @@ fun SearchMovieScreen(
     openMovieDetailScreen: (id: Int) -> Unit
 ) {
 
+    val scope = rememberCoroutineScope()
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val gridState = rememberLazyGridState()
     val shouldFetchNextPage by remember {
@@ -54,6 +57,9 @@ fun SearchMovieScreen(
                     lastVisibleIndex >= uiState.uiModel.movies.size - 10 &&
                     uiState.uiModel.canLoadMore
         }
+    }
+    val isScrollButtonVisible by remember {
+        derivedStateOf { gridState.firstVisibleItemIndex > 0 }
     }
     LaunchedEffect(shouldFetchNextPage) {
         if (shouldFetchNextPage) {
@@ -70,68 +76,87 @@ fun SearchMovieScreen(
         LoadingDialog()
     }
 
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
+    Box(modifier = Modifier.fillMaxSize()) {
 
-        OutlinedTextField(
-            value = uiState.query,
-            onValueChange = {
-                viewModel.updateUiEvents(SearchMovieEvents.OnSearchQueryChange(it))
-            },
-            label = {
-                Text(
-                    text = stringResource(Res.string.search_placeholder)
-                )
-            },
-            maxLines = 1,
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
-            keyboardOptions = KeyboardOptions(
-                imeAction = ImeAction.Search
-            ),
-            keyboardActions = KeyboardActions(
-                onSearch = {
-                    viewModel.updateUiEvents(SearchMovieEvents.OnSearch)
-                }
-            )
-        )
-        LazyVerticalGrid(
+
+        Column(
             modifier = modifier.fillMaxSize(),
-            columns = GridCells.Fixed(2),
-            state = gridState,
-            contentPadding = PaddingValues(horizontal = 8.dp, vertical = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(4.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            items(uiState.uiModel.movies, key = { it.id ?: 0 }) { movie ->
-                MovieListItem(
-                    movie = movie,
-                    onFavoriteClick = {
-                        viewModel.onFavoriteClicked(movie)
-                    },
-                    onItemClick = {
-                        openMovieDetailScreen.invoke(movie.id ?: 0)
+            CustomTopBar(
+                title = stringResource(Res.string.search_title),
+                shouldShowBack = false
+
+            )
+            OutlinedTextField(
+                value = uiState.query,
+                onValueChange = {
+                    viewModel.updateUiEvents(SearchMovieEvents.OnSearchQueryChange(it))
+                                },
+                label = {
+                    Text(
+                        text = stringResource(Res.string.search_placeholder)
+                    )
+                        },
+                maxLines = 1,
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Search
+                ),
+                keyboardActions = KeyboardActions(
+                    onSearch = {
+                        viewModel.updateUiEvents(SearchMovieEvents.OnSearch)
                     }
-                )
-            }
-            item(span = {
-                GridItemSpan(maxLineSpan)
-            }
+                ),
+                shape = RoundedCornerShape(10.dp)
+            )
+            LazyVerticalGrid(
+                modifier = modifier.fillMaxSize(),
+                columns = GridCells.Fixed(2),
+                state = gridState,
+                contentPadding = PaddingValues(horizontal = 8.dp, vertical = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
-                if (uiState.isPaginating) {
-                    Box(
-                        modifier = Modifier.fillMaxWidth().height(100.dp),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        CircularProgressIndicator(
-                            color = Color.Red
-                        )
+                items(uiState.uiModel.movies, key = { it.id ?: 0 }) { movie ->
+                    MovieListItem(
+                        movie = movie,
+                        onFavoriteClick = {
+                            viewModel.onFavoriteClicked(movie)
+                                          },
+                        onItemClick = {
+                            openMovieDetailScreen.invoke(movie.id ?: 0)
+                        }
+                    )
+                }
+                item(span = {
+                    GridItemSpan(maxLineSpan)
+                }
+                ) {
+                    if (uiState.isPaginating) {
+                        Box(
+                            modifier = Modifier.fillMaxWidth().height(100.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            CircularProgressIndicator(
+                                color = Color.Red
+                            )
+                        }
                     }
+                }
+            }
+
+        }
+        AnimatedVisibility(
+            isScrollButtonVisible, modifier = Modifier
+                .align(Alignment.BottomEnd).padding(end = 12.dp, bottom = 24.dp)
+        ) {
+            ListResetButton {
+                scope.launch {
+                    gridState.animateScrollToItem(0)
                 }
             }
         }
-
     }
 
 }
